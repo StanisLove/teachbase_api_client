29f26431c9a0489ea6b6ee485ddad5f8b8ac1fe6
SA <stankra@yandex.ru>
2016-08-08 07:58:26 +0300

    Rename application

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module List
+module TeachbaseApiClient
   class Application < Rails::Application
     config.generators do |g|
       g.test_framwork :rspec,
